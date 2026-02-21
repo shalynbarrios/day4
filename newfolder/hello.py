736c984a53c22443809714c509e4bdb1de2aecf2
Shalyn Barrios <165353062+shalynbarrios@users.noreply.github.com>
2026-02-21 09:36:58 -0600

    hello

---
+++ newfolder/hello.py
@@ -0,0 +1,8 @@
+# Hello!
+
+# Name : Shay
+# NEIGHBOR NAME : Billy
+# Neighbor's Intended major and why : Computer Engineering b/c I like  
+# solving problems
+# maybe you did a program and liked it!
+# maybe you watched Iron Man and wanted to become a SWE!
